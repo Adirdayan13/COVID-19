5f3b00f1354ff230e1da16a6b06fc3039fd908ed
Adir Dayan <adirdayan@gmail.com>
2020-04-16 19:29:42 +0200

    favicon + title + background

--- server.js
+++ server.js
@@ -17,36 +17,22 @@ if (!dev) {
   app.use(morgan("common"));
 
   app.use(express.static(path.resolve(__dirname, "build")));
-  app.get("/api/worldwide", (req, res) => {
-    fetch("https://api.covid19api.com/summary")
-      .then(res => res.text())
-      .then(body => res.json(JSON.parse(body)));
-  });
-
-  app.get("/api/:country", (req, res) => {
-    fetch(`https://api.covid19api.com/total/country/${req.params.country}`)
-      .then(res => res.text())
-      .then(body => res.json(JSON.parse(body)));
-  });
-  app.use("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
 }
 
 if (dev) {
   app.use(morgan("dev"));
-  app.get("/api/worldwide", (req, res) => {
-    fetch("https://api.covid19api.com/summary")
-      .then(res => res.text())
-      .then(body => res.json(JSON.parse(body)));
-  });
-
-  app.get("/api/:country", (req, res) => {
-    fetch(`https://api.covid19api.com/total/country/${req.params.country}`)
-      .then(res => res.text())
-      .then(body => res.json(JSON.parse(body)));
-  });
 }
+app.get("/api/worldwide", (req, res) => {
+  fetch("https://api.covid19api.com/summary")
+    .then(res => res.text())
+    .then(body => res.json(JSON.parse(body)));
+});
+
+app.get("/api/:country", (req, res) => {
+  fetch(`https://api.covid19api.com/total/country/${req.params.country}`)
+    .then(res => res.text())
+    .then(body => res.json(JSON.parse(body)));
+});
 
 const server = createServer(app);
 server.listen(port, err => {
